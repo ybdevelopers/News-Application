be3086583c9389ccb5f3c4c62d4b743ab3eea970
Yash Bhatt <70092000+ybdevelopers@users.noreply.github.com>
2022-01-22 20:26:05 +0530

    Added Navigation Componenent
    - Created Navigation Component and added all required fragments in respective package.
    - Created Auth Interceptor class for API call.
    - Created Basic Design for new listing screen.

--- app/src/main/java/com/ybdevelopers/newsapplication/MainActivity.kt
+++ app/src/main/java/com/ybdevelopers/newsapplication/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.ybdevelopers.newsapplication
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.navHostFragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
